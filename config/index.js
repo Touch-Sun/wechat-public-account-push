d493fb8020a12100e8111a749d72258cc35d6d52
lee.li <lige1204@>
2022-09-11 12:35:35 +0800

    联动值

--- config/index.js
+++ config/index.js
@@ -12,7 +12,8 @@ const res = await axios.get(api, {
   responseType: 'json'
 }).catch(err => err);
 score = res.data.data;
-console.log(score);
+lee = score[0].score
+willow = score[0].willow
 
 export const config = {
   /**
@@ -46,8 +47,8 @@ export const config = {
    */
   USERS: [
     {
-      score_lee: score,
-      score_willow: score,
+      score_lee: lee,
+      score_willow: willow,
       // 想要发送的人的名字
       name: "亲爱的媳妇儿",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
